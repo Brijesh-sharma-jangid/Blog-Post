3a4bc444fde77d44234b4dc5cf41c9d5e6b4d1cf
Brijesh <kanujangid124@gmail.com>
2023-06-15 22:44:52 +0530

    Database

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 def home(request):
-    return render(request, 'app/home.html')
+    post = Post.objects.all()
+    return render(request, 'app/home.html', {
+        'Post' : post
+    })
 
 def about(request):
     return render(request, 'app/about.html', {'title':"msg"})
